19f4d772c0ed353c04c69bf976adb48055f965a7
Amiir-sj <amirsajjad7988@gmail.com>
2025-06-09 16:29:27 +0330

    Add files via upload

--- apptwo.js
+++ apptwo.js
@@ -67,7 +67,6 @@ const questions = [
 ]
 
 const hTow = document.querySelector("h2")
-//console.log(hTow)
 const nextBtn = document.querySelector(".next-btn")
 let counter = 0
 const a = document.querySelector("#a")  || ""
@@ -75,7 +74,6 @@ const b = document.querySelector("#b")  || ""
 const c = document.querySelector("#c")  || ""
 const d = document.querySelector("#d")  || ""
 let currentQuestion = questions[counter] 
-//console.log(a.innerText)
 const ol = document.querySelector("ol")
 let score = 0
 let answered = false
@@ -84,45 +82,25 @@ const lis = document.querySelectorAll("li")
 console.log(lis)
 console.log(typeof(lis))
 
-//const lis = ol.children
-//console.log(lis)
-//console.log(typeof(lis))
-
-
 render()
 
 
 nextBtn.addEventListener ('click', () => {
-    //console.log("next")
     answered = false
-
     colorCleaner()
     render()
 })
 
-// console.log(questions)
-/* questions.forEach(quest => {
-    console.log(quest.question)
-
-}) */
-
 function render() {
     if (counter < questions.length ){
-        /* let */ currentQuestion = questions[counter]      // move declaring out of function (to top of code)
-        //console.log(currentQuestion[`question`])
+        currentQuestion = questions[counter]      
         let currentQuestionText = currentQuestion[`question`]
-        hTow.innerText = currentQuestionText
-        //console.log(currentQuestion[`a`] )
-        
+        hTow.innerText = currentQuestionText        
         a.innerText = currentQuestion[`a`]
         b.innerText = currentQuestion[`b`]
         c.innerText = currentQuestion[`c`]
         d.innerText = currentQuestion[`d`]
-        //console.log(a.innerText)
-        //console.log(currentQuestion[`a`] )
-
     } else {
-        //console.log("finish. your resulte : ")
         hTow.innerText = "finish"
         ol.innerHTML = `<li> your score: ${score} </li>`
         nextBtn.style.display = "none"
@@ -134,31 +112,13 @@ function render() {
 
 ol.addEventListener('click', (e) => {
     if (answered === false) {
-    //    ol.forEach(() => {
-    //        if (element.innerText === currentQuestion[`answer`]) {
-    //            element.classList.add("green")
-    //       }
-    //    });
-
-    // lis.forEach (li => {
-    //            if (li.innerText === currentQuestion[`answer`]) {
-    //                li.classList.add("green")
-    //            }
-    //        })
-
-    //for (const child of ol.children) {
-        //if (child.innerText === currentQuestion[`answer`] ) {
-        //    child.classList.add.green
-        //}  
 
         for (let key in lis) {
             if (lis[key].innerText === currentQuestion[`answer`] ) {
                 lis[key].classList.add("green")
             }
         }
-            
-        
-    
+
         if ( e.target.innerText === currentQuestion[`answer`] ) {
             //console.log("correct")
             score++
